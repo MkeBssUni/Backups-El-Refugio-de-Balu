a43abe81e0256f82b5a5ad7b071ab222fb7b9654
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-29 15:56:27 -0600

    Auto backup

---
+++ autoBackups.bat
@@ -0,0 +1,23 @@
+$folder = 'D:\School\BackupsBalu\bakcups'
+$filter = '*.*'
+
+$fsw = New-Object IO.FileSystemWatcher $folder, $filter -Property @{
+    IncludeSubdirectories = $false
+    NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
+}
+
+Register-ObjectEvent $fsw Created -SourceIdentifier FileCreated -Action {
+    $path = $Event.SourceEventArgs.FullPath
+    $name = $Event.SourceEventArgs.Name
+    $changeType = $Event.SourceEventArgs.ChangeType
+    $timeStamp = $Event.TimeGenerated
+    Write-Host "File $name was $changeType at $timeStamp"
+    
+    # ejecutar un git add .
+    # ejecutar un git commit -m "mensaje"
+    # ejecutar un git push
+
+    git add .
+    git commit -m "Auto backup"
+    git push
+}
