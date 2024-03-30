a2ae9a21f3bdf385453426512241517d231484a3
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-29 19:02:54 -0600

    copaCopleta

--- copiaCompleta.bat
+++ copiaCompleta.bat
@@ -1,11 +1,17 @@
 @echo off
+
+:: Log file
+:: set logFile=D:\School\BackupsBalu\backups\backup_log.txt
+
 for /f "tokens=2 delims==" %%i in ('wmic OS Get localdatetime /value') do set fecha=%%i
 set fecha=%fecha:~0,4%-%fecha:~4,2%-%fecha:~6,2%_%fecha:~8,2%_%fecha:~10,2%
 
 set nombreArchivo=D:\School\BackupsBalu\backups\fullcopies\backup_%fecha%.sql
-docker exec ecc3338f9726 mysqldump -u balu -ps2nd0b4lu refugio_balu > "%nombreArchivo%"
 
-git add backups/fullcopies/
+docker exec ecc3338f9726 mysqldump -u balu -ps2nd0b4lu refugio_balu > "%nombreArchivo%" 2>> "%logFile%"
+
+cd /d D:\School\BackupsBalu\backups\
+
+git add fullcopies/
 git commit -m "Backup %fecha%"
 git push
-
