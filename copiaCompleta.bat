42d783dd78df55b64375030212f6f4a777442f4f
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-29 18:31:50 -0600

    .

--- copiaCompleta.bat
+++ copiaCompleta.bat
@@ -2,10 +2,10 @@
 for /f "tokens=2 delims==" %%i in ('wmic OS Get localdatetime /value') do set fecha=%%i
 set fecha=%fecha:~0,4%-%fecha:~4,2%-%fecha:~6,2%_%fecha:~8,2%_%fecha:~10,2%
 
-set nombreArchivo=D:\School\BackupsBalu\backups\backup_%fecha%.sql
+set nombreArchivo=D:\School\BackupsBalu\backups\fullcopies\backup_%fecha%.sql
 docker exec ecc3338f9726 mysqldump -u balu -ps2nd0b4lu refugio_balu > "%nombreArchivo%"
 
-git add backups/
+git add backups/fullcopies/
 git commit -m "Backup %fecha%"
 git push
 
