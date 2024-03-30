41e82fb6a90750dd027f6de9300efe805ed816d2
J. Miguel Aguario D <i20213tn095@utez.edu.mx>
2024-03-29 19:34:29 -0600

    copiaComepleta.bat

--- copiaCompleta.bat
+++ copiaCompleta.bat
@@ -2,13 +2,17 @@
 
 :: Log file
 :: set logFile=D:\School\BackupsBalu\backups\backup_log.txt
+:: Ensure the Docker container is running before executing the dump
+docker start ecc3338f9726
+
+timeout /t 120 /nobreak
 
 for /f "tokens=2 delims==" %%i in ('wmic OS Get localdatetime /value') do set fecha=%%i
 set fecha=%fecha:~0,4%-%fecha:~4,2%-%fecha:~6,2%_%fecha:~8,2%_%fecha:~10,2%
 
 set nombreArchivo=D:\School\BackupsBalu\backups\fullcopies\backup_%fecha%.sql
 
-docker exec ecc3338f9726 mysqldump -u balu -ps2nd0b4lu refugio_balu > "%nombreArchivo%" 2>> "%logFile%"
+docker exec ecc3338f9726 mysqldump -u balu -ps2nd0b4lu refugio_balu > "%nombreArchivo%"
 
 cd /d D:\School\BackupsBalu\backups\
 
